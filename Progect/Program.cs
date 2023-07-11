d335a9c246265dd77d80b1e409d61eb512cc779e
NIKKnak <nik88350759@yandex.ru>
2023-07-11 17:02:42 +0300

    progect3

--- Progect/Program.cs
+++ Progect/Program.cs
@@ -7,41 +7,90 @@ namespace KR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Animals[] animals =
+            {
+                new Animals(1,"Тузик", "01.01.2010", "сидеть")
+            };
+
+
+            Console.WriteLine(animals.);
+
+
+
+
+
         }
-
-    }
-    
-
-    class Animals
-    {
-        protected int _petId;
-        protected string _name;
-        protected string _birthday;
-        protected string _commands;
+            
 
 
-        public Animals(int petId, string name, string birthday, string commands)
-        {
-            _petId = petId;
-            _name = name;
-            _birthday = birthday;
-            _commands = commands;
-        }
-
-
-        public void ShowInfo()
-        {
-            console.WriteLine($"id питомца - {_petId}, Имя - {_name}, Дата рождения - {_birthday}, Знает комманды - {_commands} " )
-        }
-
-        
         
 
 
+
+
+
+
+        class Animals
+        {
+            protected int _petId;
+            protected string _name;
+            protected string _birthday;
+            protected string _commands;
+
+
+            public Animals(int petId, string name, string birthday, string commands)
+            {
+                _petId = petId;
+                _name = name;
+                _birthday = birthday;
+                _commands = commands;
+            }
+
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"id питомца - {_petId}, Имя - {_name}, Дата рождения - {_birthday}, Знает комманды - {_commands} ");
+            }
+
+
+        }
+
+
+
+
+
+
+        public class Cat
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
