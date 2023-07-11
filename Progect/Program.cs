b7b457a2e07645cef819087c75d6588936d5577a
NIKKnak <nik88350759@yandex.ru>
2023-07-11 14:15:26 +0300

    progect1

--- Progect/Program.cs
+++ Progect/Program.cs
@@ -1,23 +1,47 @@
 ﻿
 
-
-public class Animals
+namespace KR
 {
-    protected int petId;
-    protected string name;
-    protected  birthday;
-    protected string commands;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+    }
+
+
+    class Animals
+    {
+        protected int _petId;
+        protected string _name;
+        protected string _birthday;
+        protected string _commands;
+
+
+        public Animals(int petId, string name, string birthday, string commands)
+        {
+            _petId = petId;
+            _name = name;
+            _birthday = birthday;
+            _commands = commands;
+        }
+
+
+        public void ShowInfo()
+        {
+            console.WriteLine($"id питомца - {_petId}, Имя - {_name}, Дата рождения - {_birthday}, Знает комманды - {_commands} " )
+        }
+
+        
+
+
+
+    }
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-public class Cat 
